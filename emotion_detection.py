81f6bff0b670ddd73582d8c2cf201bac79e8ede4
Elif Saracoglu <elif.saracoglu@lutech.it>
2025-08-08 05:40:50 -0400

    Structured Output Formatting

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
@@ -11,6 +12,32 @@ def emotion_detector(text_to_analyze):
             "text": text_to_analyze
         }
     }
-    
+
     response = requests.post(url, headers=headers, json=payload)
-    return response.text
+    result = json.loads(response.text)
+    emotions = result['emotionPredictions'][0]['emotion']
+
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+
+    scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+
+    dominant_emotion = max(scores, key=scores.get)
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
